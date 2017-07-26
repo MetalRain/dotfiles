0d94088669ae621eec9de065fd83625b754bbfec
Otto Martikainen <otto@qvik.fi>
2017-07-26 19:24:11 +0300

    Update xmonad statusbar and git aliases

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,28 @@
 import XMonad
+import XMonad.Config
+import System.IO
+import XMonad.Util.Run
+import XMonad.Config.Desktop
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.ResizableTile 
 
-main = xmonad $ defaultConfig
-  { terminal = "xterm"
-  , normalBorderColor = "black"
-  , focusedBorderColor = "green"
+main = do
+  xmonad $ desktopConfig
+    { terminal = "xterm"
+    , normalBorderColor = "black"
+    , focusedBorderColor = "green"
+    , modMask = mod4Mask
+    , manageHook = myManageHook
+    , layoutHook = myLayoutHook
+    }
+
+myManageHook = manageDocks <+> manageHook desktopConfig
+
+myLogHook h = dynamicLogWithPP $ defaultPP 
+  { ppOutput = hPutStrLn h
   }
+
+myLayoutHook = avoidStruts $ tiled ||| Full
+  where tiled = Tall 1 (2/100) (3/5)
+  
